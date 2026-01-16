1d9a7557d66323a2b001970b06b459276f523caf
harel <harel8090@gmail.com>
2026-01-16 17:34:28 +0200

    documentation

--- src/PacMan/Ex3Algo.java
+++ src/PacMan/Ex3Algo.java
@@ -67,17 +67,29 @@ public class Ex3Algo implements PacManAlgo{
 			System.out.println(i+") status: "+g.getStatus()+",  type: "+g.getType()+",  pos: "+g.getPos(0)+",  time: "+g.remainTimeAsEatable(0));
 		}
 	}
-	private static int randomDir() {
+	private static int randomDir() { //left unused
 		int[] dirs = {Game.UP, Game.LEFT, Game.DOWN, Game.RIGHT};
 		int ind = (int)(Math.random()*dirs.length);
 		return dirs[ind];
 	}
+
+    /**
+     * This is the main algorithm that chooses which way the pacman moves every turn
+     * first it finds where the ghosts are and places barriers on them and around them to prevent the pacman from pathing through them
+     * then it callse closestPink() to find out where the nearest reachable pink is located while considering walls and ghosts as barriers
+     * then calls shortestPath() to find out which way to navigate
+     * shortestPath() returns an array with the tiles referencing the optimal path so the 0 spot is the origin point and the 1 spot is where the pacman needs to move
+     * the algorithm finds out where the path[1] is in reference to the pacman and returns which direction the pacman needs to move to reach it
+     * if there is no way to reach a pink the pacman does not move
+     * @param game
+     * @return direction to move as represented by an integer
+     */
     private static int moveSelect(PacmanGame game){
         PacMan2.Map board = new Map(game.getGame(0));
         int blue = Game.getIntColor(Color.BLUE, 0);
         GhostCL[] ghosts = game.getGhosts(0);
-        if (ghosts[0].remainTimeAsEatable(0) < 0.5) {
-            for (int i = 0; i < ghosts.length; i++) {
+        if (ghosts[0].remainTimeAsEatable(0) < 0.3) { //if the ghosts are edible there is no reason to consider them as a barrier
+            for (int i = 0; i < ghosts.length; i++) { //for each ghost it will place a barrier on them and on each nearest tile in the 4 cardinals
                 PacMan2.Pixel2D ghost = new PacMan2.Index2D(Integer.parseInt(ghosts[i].getPos(0).split(",")[0]), Integer.parseInt(ghosts[i].getPos(0).split(",")[1]));
                 board.setPixel(ghost, blue);
                 if (ghost.getX() != 0) {
@@ -114,7 +126,7 @@ public class Ex3Algo implements PacManAlgo{
         PacMan2.Pixel2D Pac = new PacMan2.Index2D(Integer.parseInt(pos.split(",")[0]),Integer.parseInt(pos.split(",")[1]));
         PacMan2.Pixel2D closest = closestPink(game);
         PacMan2.Pixel2D[] path = board.shortestPath(Pac,closest,blue);
-        if ((path == null)||(path.length==1)) {
+        if ((path == null)||(path.length==1)) { //if there is no path to a pink the pacman wont move
             return 0;
         }
         if (Pac.getX() != 0) {
@@ -155,14 +167,21 @@ public class Ex3Algo implements PacManAlgo{
         }
         return 0;
     }
+
+    /**
+     * This is an external function to help moveSelect()
+     * this function uses BFS fill algorithm to find the nearest pink and returns its position as a Pixel2D
+     * @param game
+     * @return
+     */
     private static PacMan2.Pixel2D closestPink(PacmanGame game){
         String pos = game.getPos(0);
         GhostCL[] ghosts = game.getGhosts(0);
         int[][] board = game.getGame(0);
         int blue = Game.getIntColor(Color.BLUE, 0);
         int pink = Game.getIntColor(Color.PINK, 0);
-        if (ghosts[0].remainTimeAsEatable(0) < 0.5) {
-            for (int i = 0; i < ghosts.length; i++) {
+        if (ghosts[0].remainTimeAsEatable(0) < 0.3) { //ignore ghosts if they are still edible
+            for (int i = 0; i < ghosts.length; i++) { //for each ghost place barriers on them and next to them in each cardinal direction
                 PacMan2.Pixel2D ghost = new PacMan2.Index2D(Integer.parseInt(ghosts[i].getPos(0).split(",")[0]), Integer.parseInt(ghosts[i].getPos(0).split(",")[1]));
                 board[ghost.getX()][ghost.getY()] = blue;
                 if (ghost.getX() != 0) {
@@ -199,7 +218,7 @@ public class Ex3Algo implements PacManAlgo{
         Queue<PacMan2.Pixel2D> queue = new LinkedList<>();
         queue.add(Pac);
         PacMan2.Pixel2D ans;
-        while(queue.peek() != null){
+        while(queue.peek() != null){ //BFS algorithm using queue. leaves behind simulated walls (blue) to not repeat steps and returns the first pink found or returns the pacman's position if there is no reachable pink
             if (queue.peek().getX() != 0){
                 if (board[queue.peek().getX()-1][queue.peek().getY()] == pink){
                     ans = new PacMan2.Index2D(queue.peek().getX()-1,queue.peek().getY());
